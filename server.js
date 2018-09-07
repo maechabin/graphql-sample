fa85b5d5091a31d5082c1d8a1945315c70b7a4a2
maechabin <mitoppo@gmail.com>
2018-09-07 23:23:11 +0900

    mongodb導入

--- server.js
+++ server.js
@@ -3,6 +3,29 @@ const cors = require('cors');
 const graphqlHTTP = require('express-graphql');
 const { buildSchema } = require('graphql');
 
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'myproject';
+
+// Use connect method to connect to the server
+MongoClient.connect(
+  url,
+  { useNewUrlParser: true },
+  (err, client) => {
+    assert.equal(null, err);
+    console.log('Connected successfully to server');
+
+    const db = client.db(dbName);
+
+    client.close();
+  },
+);
+
 let data = require('./data.json');
 
 const schema = buildSchema(`
